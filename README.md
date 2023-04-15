96b2e6bbdb8948bd60aa7544ebfb10deccb40758
Wallace <yuzhuo.w@gmail.com>
2023-04-15 12:59:54 +0100

    so far so good

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # chess-club-admin-system
+
+Using data provided by the chess.com public API to facilitate a range of typical admin tasks.
---
+++ src/utils/constants.py
@@ -0,0 +1,9 @@
+from database.database_utils import path
+
+HEADERS = {
+    "Accept": "application/json",
+    "From": "yuzhuo.w@gmail.com",
+    "User-Agent": "Wallace_Wang",
+}
+
+print(path)
