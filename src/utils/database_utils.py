1b7b45de3cc80d03948c46a1d45ea7faa266f39f
Wallace <yuzhuo.w@gmail.com>
2023-05-08 23:01:52 +0800

    clarity

--- src/utils/database_utils.py
+++ src/utils/database_utils.py
@@ -1,6 +1,6 @@
 import sqlite3
 
 
-def get_cursor(club_url_name: str) -> sqlite3.Cursor:
-    con = sqlite3.connect(f"databases/{club_url_name}.db")
+def get_cursor(club_name: str) -> sqlite3.Cursor:
+    con = sqlite3.connect(f"databases/{club_name}.db")
     return con.cursor()
