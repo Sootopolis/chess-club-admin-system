7f7db87f959c3a8dbda8d58617d2a9ae892598d1
wallace <yuzhuo.w@gmail.com>
2023-09-12 10:05:25 +0100

    updated records

--- src/utils/alchemy.py
+++ src/utils/alchemy.py
@@ -1,7 +1,8 @@
+from typing import Optional
 from sqlalchemy import TIMESTAMP, MetaData
 from sqlalchemy.orm import DeclarativeBase, Mapped, MappedAsDataclass, mapped_column
 
-from .alchemy_enums import CandidateStatus, MatchStatus
+from .alchemy_enums import CandidateStatus, GameResult, MatchStatus
 
 
 class Base(DeclarativeBase, MappedAsDataclass):
@@ -40,12 +41,25 @@ class ClubORM(Base):
     url_name: Mapped[str] = mapped_column(nullable=False)
 
 
-class ClubMatch(Base):
-    __tablename__ = "club_match"
-    match_id: Mapped[int] = mapped_column(primary_key=True)
+class DailyMatchORM(Base):
+    __tablename__ = "daily_match"
+    match_id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
     status: Mapped[MatchStatus] = mapped_column(nullable=False)
     start_time: Mapped[int] = mapped_column(TIMESTAMP, nullable=True)
     end_time: Mapped[int] = mapped_column(TIMESTAMP, nullable=True)
+
+
+class DailyBoardORM(Base):
+    __tablename__ = "daily_board"
+    match_id: Mapped[int] = mapped_column(primary_key=True)
+    board_number: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(nullable=False)
+    start_time: Mapped[Optional[int]] = mapped_column(TIMESTAMP, nullable=True)
+    end_time: Mapped[Optional[int]] = mapped_column(TIMESTAMP, nullable=True)
+    url_white: Mapped[Optional[str]] = mapped_column(nullable=True)
+    url_black: Mapped[Optional[str]] = mapped_column(nullable=True)
+    result_white: Mapped[Optional[GameResult]] = mapped_column(nullable=True)
+    result_black: Mapped[Optional[GameResult]] = mapped_column(nullable=True)
 
 
 wallace = MemberORM(0, "wallace", 0, True)
--- src/utils/alchemy_enums.py
+++ src/utils/alchemy_enums.py
@@ -1,7 +1,7 @@
-from enum import StrEnum, auto
+from enum import Enum, StrEnum, auto
 
 
-class CandidateStatus(StrEnum):
+class CandidateStatus(Enum):
     JOINED = auto()
     INVITED = auto()
     TIMEOUT = auto()
@@ -11,4 +11,41 @@ class CandidateStatus(StrEnum):
 class MatchStatus(StrEnum):
     FINISHED = auto()
     IN_PROGRESS = auto()
-    REGISTERED = auto()
+    REGISTRATION = auto()
+
+
+class GameResultCategory(float, Enum):
+    WIN = 1.0
+    DRAW = 0.5
+    LOSS = 0.0
+
+
+class GameResult(Enum):
+    WIN = "win"
+    CHECKMATE = "checkmated"
+    AGREEMENT = "agreed"
+    REPETITION = "repetition"
+    TIMEOUT = "timeout"
+    RESIGNATION = "resigned"
+    STALEMATE = "stalemate"
+    LOSS = "lose"
+    INSUFFICIENT = "insufficient"
+    FIFTY_MOVE = "50move"
+    TIMEOUT_INSUFFICIENT = "timevsinsufficient"
+    DRAW = "draw"
+
+
+results: dict[GameResult, GameResultCategory] = {
+    GameResult.WIN: GameResultCategory.WIN,
+    GameResult.DRAW: GameResultCategory.DRAW,
+    GameResult.AGREEMENT: GameResultCategory.DRAW,
+    GameResult.REPETITION: GameResultCategory.DRAW,
+    GameResult.STALEMATE: GameResultCategory.DRAW,
+    GameResult.INSUFFICIENT: GameResultCategory.DRAW,
+    GameResult.FIFTY_MOVE: GameResultCategory.DRAW,
+    GameResult.TIMEOUT_INSUFFICIENT: GameResultCategory.DRAW,
+    GameResult.LOSS: GameResultCategory.LOSS,
+    GameResult.CHECKMATE: GameResultCategory.LOSS,
+    GameResult.TIMEOUT: GameResultCategory.LOSS,
+    GameResult.RESIGNATION: GameResultCategory.LOSS,
+}
